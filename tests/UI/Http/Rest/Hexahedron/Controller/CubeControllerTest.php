01eeeb7800e92c9c7c54a37ca738bd2cbbf90413
oscarlopez1616 <oscar.lopez.labrador@gmail.com>
2018-03-02 18:28:37 +0100

    clover coberture

--- tests/UI/Http/Rest/Hexahedron/Controller/CubeControllerTest.php
+++ tests/UI/Http/Rest/Hexahedron/Controller/CubeControllerTest.php
@@ -22,9 +22,9 @@ class CubeControllerTest extends WebTestCase
     public function dataProvider()
     {
         return array(
-            array('/api/cube/volume?x1=0&y1=0&z1=0&edge1=3&x2=3&y2=0&z2=0&edge2=3', 200, '{"unit":"cm","meassure":0}'),
-            array('/api/cube/volume?x1=0&y1=0&z1=0&edge1=3&x2=0&y2=0&z2=0&edge2=3', 200, '{"unit":"cm","meassure":27}'),
-            array('/api/cube/volume?x1=0&y1=0&z1=0&edge1=3&x2=1&y2=0&z2=0&edge2=3', 200, '{"unit":"cm","meassure":18}')
+            array('/api/cube/volume?x1=0&y1=0&z1=0&edge1=3&x2=3&y2=0&z2=0&edge2=3', 200, '{"unit":"m3","meassure":0}'),
+            array('/api/cube/volume?x1=0&y1=0&z1=0&edge1=3&x2=0&y2=0&z2=0&edge2=3', 200, '{"unit":"m3","meassure":27}'),
+            array('/api/cube/volume?x1=0&y1=0&z1=0&edge1=3&x2=1&y2=0&z2=0&edge2=3', 200, '{"unit":"m3","meassure":18}')
         );
     }
 }
